0e4d09b1d9fb95c5e6c0f2247a6f9d2644dc1a13
陳泓維 <tim5556618@gmail.com>
2017-11-26 19:22:19 +0800

    刪除資料

--- routes/web.php
+++ routes/web.php
@@ -15,42 +15,46 @@ Route::get('/', function () {
     return view('welcome');
     });
 
-Route::get('/test',funtion(){
+Route::get('/test',function() {
+
+
+    \App\Post::create([
+        'title' => 'test title',
+        'content' => 'test content',
+    ]);
+
+    $post = new \App\Post();
+    $post->title = 'test title';
+    $post->content = 'test content';
+    $post->save();
+
+
+    $posts = \App\Post::all();
+    dd($posts);
+
+    \App\Post::find(1);
+    dd($post);
+
+    \App\Post::where('id', '<', 10)->orderBy('id', 'DESC')->get();
+    dd($posts);
+
+    /* $post=\App\Post::find(1);
+    $post‐>update([
+        'title'=>'updatedtitle',
+        'content'=>'updatedcontent',
+    ]);
+    */
+
+    $post = \App\Post::find(1);
+    $post->title = 'savedtitle';
+    $post->content = 'savedcontent';
+    $post->save();
+
+    $post = \App\Post::find(1);
+    $post->delete();
+
+    \App\Post::destroy(2);
+
+    \App\Post::destroy(3, 5, 7);
 
 });
-
-\App\Post::create([
-    'title'=>'test title',
-    'content'=>'test content',
-]);
-
-$post = new \App\Post();
-$post -> title = 'test title';
-$post -> content ='test content';
-$post -> save();
-
-
-$posts = \App\Post::all();
-dd($posts);
-
-\App\Post::find(1);
-dd($post);
-
-\App\Post::where('id', '<', 10)->orderBy('id', 'DESC')->get();
-dd($posts);
-
-/* $post=\App\Post::find(1);
-$post‐>update([
-    'title'=>'updatedtitle',
-    'content'=>'updatedcontent',
-]);
-*/
-
-$post=\App\Post::find(1);
-$post‐>title='savedtitle';
-$post‐>content='savedcontent';
-$post‐>save();
-
-
-
-
